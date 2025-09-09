71862b6a228cc55ef54d24f0ebb1f8e8720a9164
araxnoid <araxnoid0@gmail.com>
2025-09-09 11:58:34 +0700

    pratikum 1 kelas c

---
+++ prakalpro_2025_2511533003/src/pekan1/HelloWorld.java
@@ -0,0 +1,15 @@
+package pekan1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("Program ini menghasilkan");
+		System.out.println("Empat baris output");
+		System.out.println("/ \\ // \\\\ /// \\\\");
+		System.out.println("This"+"program prints a\n"+"quote from the Gettysburg Address.");
+	}
+
+}
---
+++ prakalpro_2025_2511533003/src/pekan1/MembuatKue.java
@@ -0,0 +1,35 @@
+package pekan1;
+
+public class MembuatKue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		makeBatter();
+		memanggang();
+		memanggang();
+		menghias();
+	}
+
+	public static void makeBatter() {
+		System.out.println("Campur bahan kering.");
+		System.out.println("Krim mentega dan gula.");
+		System.out.println("Kocok terlurnya.");
+		System.out.println("Masukkan bahan kering.");
+
+	}
+
+	public static void memanggang() {
+		System.out.println("Setel suhu oven.");
+		System.out.println("Setel pengatur waktu.");
+		System.out.println("Masukkan kue ke dalam oven.");
+		System.out.println("Biarkan cookie untuk dipanggang.");
+
+	}
+
+	public static void menghias() {
+		System.out.println("Campur bahan untuk frosting.");
+		System.out.println("Taburkan frosting dan taburan.");
+	}
+
+}
+
