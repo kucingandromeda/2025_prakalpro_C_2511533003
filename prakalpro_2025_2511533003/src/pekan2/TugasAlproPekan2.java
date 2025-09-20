da3ecf92f2014d69da4a9d398498b61cbef0517f
araxnoid <araxnoid0@gmail.com>
2025-09-21 06:36:40 +0700

    menambahkan tugas pekan 2 dan laporan tugas pekan 2

---
+++ prakalpro_2025_2511533003/src/pekan2/TugasAlproPekan2.java
@@ -0,0 +1,48 @@
+package pekan2;
+
+public class TugasAlproPekan2{
+    public static void main(String[] args) {
+        // inisialaisasi variable
+        int urutanPasien = 1;
+        char ruangan = 'A';
+        float perkiraanUmur = 75;
+        boolean statusDapatDisembuhkan = true;
+
+        // cetak data
+        System.out.println("_____Data Pasien Kanker_____");
+        System.out.println("pasien ke\t\t\t: " + urutanPasien);
+        System.out.println("ruangan\t\t\t\t: " + ruangan);
+        System.out.println("perkiraan umur\t\t\t: " + perkiraanUmur);
+        System.out.println("status dapat disembuhkan\t: " + statusDapatDisembuhkan);
+        System.out.println("\n");
+
+        // inisialaisasi variable
+        urutanPasien = 2;
+        ruangan = 'B';
+        perkiraanUmur = 89;
+        statusDapatDisembuhkan = false;
+
+        // cetak data
+        System.out.println("_____Data Pasien Kanker_____");
+        System.out.println("pasien ke\t\t\t: " + urutanPasien);
+        System.out.println("ruangan\t\t\t\t: " + ruangan);
+        System.out.println("perkiraan umur\t\t\t: " + perkiraanUmur);
+        System.out.println("status dapat disembuhkan\t: " + statusDapatDisembuhkan);
+        System.out.println("\n");
+
+        // inisialaisasi variable
+        urutanPasien = 3;
+        ruangan = 'C';
+        perkiraanUmur = 63;
+        statusDapatDisembuhkan = false;
+
+        // cetak data
+        System.out.println("_____Data Pasien Kanker_____");
+        System.out.println("pasien ke\t\t\t: " + urutanPasien);
+        System.out.println("ruangan\t\t\t\t: " + ruangan);
+        System.out.println("perkiraan umur\t\t\t: " + perkiraanUmur);
+        System.out.println("status dapat disembuhkan\t: " + statusDapatDisembuhkan);
+        System.out.println("\n");
+    }
+}
+
